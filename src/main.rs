fba3f70840051935c7eb9b5fefa25c2e49d88403
jdivass <julianandre.divas@gmail.com>
2026-07-20 00:24:46 -0600

    Correct only poligon in branch 2

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,6 @@ fn main() {
     let mut framebuffer = Framebuffer::new(800, 600, Color::BLACK);
     framebuffer.clear();
 
-    let polygon1 = [
-        Vector2::new(165.0, 380.0),
-        Vector2::new(185.0, 360.0),
-        Vector2::new(180.0, 330.0),
-        Vector2::new(207.0, 345.0),
-        Vector2::new(233.0, 330.0),
-        Vector2::new(230.0, 360.0),
-        Vector2::new(250.0, 380.0),
-        Vector2::new(220.0, 385.0),
-        Vector2::new(205.0, 410.0),
-        Vector2::new(193.0, 383.0),
-    ];
-
-    framebuffer.set_current_color(Color::YELLOW);
-    fill_polygon(&mut framebuffer, &polygon1);
-
-    framebuffer.set_current_color(Color::WHITE);
-    draw_polygon(&mut framebuffer, &polygon1);
-
     let polygon2 = [
         Vector2::new(321.0, 335.0),
         Vector2::new(288.0, 286.0),
@@ -45,4 +26,5 @@ fn main() {
     draw_polygon(&mut framebuffer, &polygon2);
 
     framebuffer.render_to_file("out.png");
+    framebuffer.render_to_bmp("out.bmp");
 }
