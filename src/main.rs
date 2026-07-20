669259821363c378de6a65f4654df6bee1c0006a
jdivass <julianandre.divas@gmail.com>
2026-07-19 23:49:45 -0600

    Finished polygon 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-mod framebuffer;
+use raylib::prelude::*;
 
-fn main(){}
+mod framebuffer;
+mod line;
+mod polygon;
+
+use framebuffer::Framebuffer;
+use polygon::draw_polygon;
+use polygon::fill_polygon;
+
+fn main() {
+    let mut framebuffer = Framebuffer::new(800, 600, Color::BLACK);
+    framebuffer.clear();
+
+
+    let polygon1 = [
+        Vector2::new(165.0, 380.0),
+        Vector2::new(185.0, 360.0),
+        Vector2::new(180.0, 330.0),
+        Vector2::new(207.0, 345.0),
+        Vector2::new(233.0, 330.0),
+        Vector2::new(230.0, 360.0),
+        Vector2::new(250.0, 380.0),
+        Vector2::new(220.0, 385.0),
+        Vector2::new(205.0, 410.0),
+        Vector2::new(193.0, 383.0),
+    ];
+
+    framebuffer.set_current_color(Color::YELLOW);
+    fill_polygon(&mut framebuffer, &polygon1);
+
+    framebuffer.set_current_color(Color::WHITE);
+    draw_polygon(&mut framebuffer, &polygon1);
+
+    framebuffer.render_to_file("out.png");
+}
--- src/polygon.rs
+++ src/polygon.rs
@@ -17,3 +17,65 @@ pub fn draw_polygon(
         line(framebuffer, start, end);
     }
 }
+
+pub fn fill_polygon(framebuffer: &mut Framebuffer, points: &[Vector2]) {
+    if points.len() < 3 {
+        return;
+    }
+
+    let min_y = points
+        .iter()
+        .map(|p| p.y as i32)
+        .min()
+        .unwrap();
+
+    let max_y = points
+        .iter()
+        .map(|p| p.y as i32)
+        .max()
+        .unwrap();
+
+    for y in min_y..=max_y {
+        let mut intersections: Vec<i32> = Vec::new();
+        for i in 0..points.len() {
+
+            let p1 = points[i];
+            let p2 = points[(i + 1) % points.len()];
+            let y1 = p1.y as i32;
+            let y2 = p2.y as i32;
+
+            if y1 == y2 {
+                continue;
+            }
+
+            let ymin = y1.min(y2);
+            let ymax = y1.max(y2);
+
+            if y >= ymin && y < ymax {
+
+                let x = p1.x
+                    + (y as f32 - p1.y)
+                        * (p2.x - p1.x)
+                        / (p2.y - p1.y);
+
+                intersections.push(x.round() as i32);
+            }
+        }
+
+        intersections.sort();
+
+        let mut i = 0;
+
+        while i + 1 < intersections.len() {
+            let x_start = intersections[i];
+            let x_end = intersections[i + 1];
+
+            for x in x_start..=x_end {
+                if x >= 0 && y >= 0 {
+                    framebuffer.set_pixel(x as u32, y as u32);
+                }
+            }
+            i += 2;
+        }
+    }
+}
